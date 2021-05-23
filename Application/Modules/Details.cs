c6297cfdbc6dcf18edcd93803a6929a69d0e47f7
Saahith-Ahamed <ahamedrs2020@gmail.com>
2021-05-23 17:54:22 +0530

    Added Details.cs and Edit.cs in modules

---
+++ Application/Modules/Details.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using ObeSystem.Models;
+using ObeSystem.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ObeSystem.Application.Modules
+{
+    public class Details
+    {
+
+        public class Query : IRequest<Module>
+        {
+            public Guid Id { get; set; }
+
+        }
+
+        public class Handler : IRequestHandler<Query, Module>
+        {
+            private readonly AppDbContext _context;
+
+            public Handler(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Module> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var module = await _context.Modules.FindAsync(request.Id);
+
+                return module;
+            }
+        }
+    }
+}
---
+++ Application/Modules/Edit.cs
@@ -0,0 +1,73 @@
+﻿using MediatR;
+using ObeSystem.Models;
+using ObeSystem.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ObeSystem.Application.Modules
+{
+    public class Edit
+    {
+
+        public class Command : IRequest<Module>
+        {
+            public Guid Id { get; set; }
+
+
+            public string Module_name { get; set; }
+
+            public string Module_code { get; set; }
+
+
+            public int? Semester { get; set; }
+
+
+            public string Lecturer { get; set; }
+
+
+            public DateTime? Date { get; set; }
+
+            public Guid? ThresholdId { get; set; }
+
+        }
+
+        public class Handler : IRequestHandler<Command, Module>
+        {
+            private readonly AppDbContext _context;
+
+            public Handler(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Module> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var newThreshold = _context.Thresholds.SingleOrDefault(c => c.Id == request.ThresholdId);
+                var module = await _context.Modules.FindAsync(request.Id);
+
+                if (module == null)
+                    throw new Exception("Could not find module");
+
+                module.Module_name = request.Module_name ?? module.Module_name;
+                module.Module_code = request.Module_code ?? module.Module_code;
+                module.Semester = request.Semester ?? module.Semester;
+                module.Lecturer = request.Lecturer ?? module.Lecturer;
+                module.Date = request.Date ?? module.Date;
+                module.ThresholdId = request.ThresholdId;
+                module.Threshold = newThreshold;
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return module;
+
+                throw new Exception("Problem saving changes");
+
+
+
+            }
+        }
+    }
+}

7d0d210418d71982696015c13a87d5cf51f584ab
Duwaraga Selvaraj <18duwa@gmail.com>
2021-05-23 16:25:32 +0530

    Updated Assessment & AssessmentLo Controller

---
+++ Controllers/AssessmentController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ObeSystem.Application.Assessments;
+using ObeSystem.Interfaces;
+using ObeSystem.Models;
+
+namespace ObeSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssessmentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AssessmentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        // GET: api/Assessment
+        [HttpGet]
+        public async Task<ActionResult<List<AssessmentDto>>> List()
+        { 
+            return await _mediator.Send(new List.Query());
+
+
+        }
+
+
+        //GET: api/Assessment/5
+        [HttpGet("{id?}")]
+        public async Task<ActionResult<AssessmentDto>> Details(Guid id)
+        {
+            return await _mediator.Send(new Details.Query { Id = id });
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Assessment>> Create(Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+
+
+        [HttpPut("{id?}")]
+        public async Task<ActionResult<Assessment>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Assessment>> Delete(Guid id)
+        {
+
+            return await _mediator.Send(new Delete.Command { Id = id });
+        }
+
+
+
+    }
+}

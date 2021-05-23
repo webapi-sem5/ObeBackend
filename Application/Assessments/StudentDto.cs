1b71a540676324cd031697b547706fa360a3b2f4
mfmnufail <mfmnufail@gmail.com>
2021-05-23 18:01:51 +0530

    Added Assessment DTO,ModuleDTO and StudentDTO in Assessment Application

---
+++ Application/Assessments/StudentDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObeSystem.Application.Assessments
+{
+    public class StudentDto
+    {
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Registration_number { get; set; }
+
+        public float Student_marks { get; set; }
+
+        public int Batch { get; set; }
+
+    }
+}

324bfa0e6f11ee74bb7c336f7a896a78e3692c0f
Duwaraga Selvaraj <18duwa@gmail.com>
2021-05-23 18:08:14 +0530

    Updated Assessment- Details,Edit and Delete in Application

---
+++ Application/Assessments/Delete.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using ObeSystem.Models;
+using ObeSystem.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ObeSystem.Application.Assessments
+{
+    public class Delete
+    {
+        public class Command : IRequest<Assessment>
+        {
+            public Guid Id { get; set; }
+
+        }
+
+        public class Handler : IRequestHandler<Command, Assessment>
+        {
+            private readonly AppDbContext _context;
+
+            public Handler(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Assessment> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var assessment = await _context.Assessments.FindAsync(request.Id);
+
+                if (assessment == null)
+                    throw new Exception("Could not find assessment");
+
+                _context.Assessments.Remove(assessment);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return assessment;
+
+                throw new Exception("Problem saving changes");
+
+
+
+            }
+        }
+    }
+}
---
+++ Application/Assessments/Details.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ObeSystem.Models;
+using ObeSystem.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ObeSystem.Application.Assessments
+{
+    public class Details
+    {
+
+        public class Query : IRequest<AssessmentDto> 
+        {
+            public Guid Id { get; set; }
+
+        }
+
+        public class Handler : IRequestHandler<Query, AssessmentDto>
+        {
+            private readonly AppDbContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(AppDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<AssessmentDto> Handle(Query request, CancellationToken cancellationToken)
+            {
+
+
+                var assessment = await _context.Assessments
+                    .Include(c => c.Module)
+                    .Include(c => c.AssessmentStudents)
+                    .ThenInclude(c => c.Student)
+                    .Include(x => x.AssessmentLos)
+                    .ThenInclude(x => x.Lolist)
+                    .SingleOrDefaultAsync(c => c.Id == request.Id);
+
+
+                var assessmentToReturn = _mapper.Map<Assessment, AssessmentDto>(assessment);
+
+                return assessmentToReturn;
+            }
+        }
+    }
+}

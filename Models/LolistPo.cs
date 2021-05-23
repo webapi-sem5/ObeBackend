2081e17c3527d85f20348dc19c74b43befbab896
mfmnufail <mfmnufail@gmail.com>
2021-05-23 16:52:25 +0530

    Added relashionships in domain class - ModuleAssessment,AssessmentLo,AssessmentStudent,LolistPo and StudentLolist

---
+++ Models/LolistPo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ObeSystem.Models
+{
+    public class LolistPo
+    {
+
+        public Guid LolistId { get; set; }
+
+        public Lolist Lolist { get; set; }
+
+       
+        public Guid PolistId { get; set; }
+
+        public Polist Polist { get; set; }
+
+
+    }
+}
